727cdc6cfe27c1d17624122dc0262d485f9a9f54
saurabh73 <mail@saurabhdutta.dev>
2022-02-19 03:01:05 +0530

    server with express and routing controller

---
+++ src/controllers/greetings.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from "routing-controllers";
+
+@Controller()
+export class GreetingsController {
+
+    @Get('/')
+    sayHello() {
+        return 'Hello World';
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { createExpressServer } from 'routing-controllers';
+const port = 3000;
+const baseDir = __dirname;
+const app = createExpressServer({
+    controllers: [baseDir + '/controllers/{*.js,*.ts}'],
+    middlewares: [baseDir + '/middlewares/{*.js,*.ts}'],
+});
+
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
